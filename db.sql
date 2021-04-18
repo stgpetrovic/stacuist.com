6732611ee4f44318692160f3e863b6644d947961
Goran Petrovic <stgpetrovic@gmail.com>
2021-04-18 21:13:08 +0200

    add ingredients

--- db.sql
+++ db.sql
@@ -1,1 +1,25 @@
-create table recipe (id integer primary key autoincrement, version integer not null, name text not null, author text not null, text text not null, tags text);
+create table recipe (
+  id integer primary key autoincrement,
+  version integer not null,
+  name text not null,
+  author text not null,
+  text text not null,
+  tags text
+);
+
+create table ingredient (
+  id integer primary key autoincrement,
+  name text not null,
+  quantity float not null
+);
+
+create table recipe_ingredient (
+  recipe_id integer not null,
+  ingredient_id integer not null,
+  primary key (recipe_id, ingredient_id),
+  constraint fk_recipe foreign key (recipe_id) references recipe (id) on delete cascade deferrable initially deferred,
+  constraint fk_ingredient foreign key (ingredient_id) references ingredient (id) on delete cascade deferrable initially deferred
+);
+
+create index recipe_ingredient_recipe on recipe_ingredient (recipe_id);
+create index recipe_ingredient_ingredient on recipe_ingredient (ingredient_id);
--- engine/recipe.h
+++ engine/recipe.h
@@ -4,6 +4,24 @@
 
 namespace stacuist::engine {
 
+class Recipe;
+
+class Ingredient {
+ public:
+  std::string name;
+  float quantity;
+
+  Wt::Dbo::collection<Wt::Dbo::ptr<Recipe> > recipes;
+
+  template <class Action>
+  void persist(Action& a) {
+    Wt::Dbo::field(a, name, "name");
+    Wt::Dbo::field(a, quantity, "quantity");
+
+    Wt::Dbo::hasMany(a, recipes, Wt::Dbo::ManyToMany, "recipe_ingredients");
+  }
+};
+
 class Recipe {
  public:
   std::string name;
@@ -11,13 +29,17 @@ class Recipe {
   std::string text;
   std::string tags;
 
+  Wt::Dbo::collection<Wt::Dbo::ptr<Ingredient> > ingredients;
+
   template <class Action>
   void persist(Action& a) {
     Wt::Dbo::field(a, name, "name");
     Wt::Dbo::field(a, author, "author");
     Wt::Dbo::field(a, text, "text");
     Wt::Dbo::field(a, tags, "tags");
+
+    Wt::Dbo::hasMany(a, ingredients, Wt::Dbo::ManyToMany, "recipe_ingredients");
   }
 };
 
-};  // namespace stacuist::engine
+}  // namespace stacuist::engine
