c5e6bad7dfe9ddb1bf5c7c989d2cdbc5519d4b03
Goran Petrovic <stgpetrovic@gmail.com>
2021-05-29 10:57:31 +0200

    --amend

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@
 # Compile the app and bundle the resources.
 CC="/usr/bin/clang++" BAZEL_CXXOPTS="-std=c++17" bazel build -c opt //:stacuist_webapp --strategy=Genrule=local
 
+ssh root@159.89.213.201 'cd ~/stacuist.com && rm -rf *'
 rsync bazel-bin/stacuist.com.tar root@159.89.213.201:~/stacuist.com/
+ssh root@159.89.213.201 'cd ~/stacuist.com && tar -xvf stacuist.com.tar'
