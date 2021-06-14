0303d9673712441eeead7f397afe6bc2c4917c76
Goran Petrovic <stgpetrovic@gmail.com>
2021-06-14 20:20:26 +0200

    rework the run script

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-until WT_APP_ROOT=/root/stacuist.com/resources /usr/bin/spawn-fcgi -n -p 9091 -- /root/stacuist.com/main --db_path /root/stacuist.com/recipes.db; do
-  echo "Server crashed, respawning..."
-  sleep 1
-  killall main
+SLEEP_IN_SECS=3600  #this is 1h
+while (( 1 ))
+do
+        ps -aef | grep main | awk '{print $2}' | while read a
+        do
+        echo -e "Killing the server..."
+        kill ${a}
+        done
+echo -e "Running the server..."
+WT_APP_ROOT=/root/stacuist.com/resources /usr/bin/spawn-fcgi -n -p 9091 -- /root/stacuist.com/main --db_path /root/stacuist.com/recipes.db &
+sleep ${SLEEP_IN_SECS}
 done
